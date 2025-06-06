46c07975f4671b5095d3f5f17c63873a1a2e7ce8
ChristopherAGT <christopherchivalan01@gmail.com>
2025-06-06 15:27:37 -0600

    Update v2ray-manager.sh

--- v2ray-manager.sh
+++ v2ray-manager.sh
@@ -21,7 +21,7 @@ function mostrar_menu() {
     while true; do
         clear
         echo -e "${green}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${nc}"
-        echo -e "${green}          PANEL DE CONTROL V2RAY-VLESS${nc}"
+        echo -e "${green}     PANEL DE CONTROL V2RAY-VLESS${nc}"
         echo -e "${green}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${nc}"
         echo -e "${yellow}1) Construir Servicio${nc}"
         echo -e "${yellow}2) Remover Servicio${nc}"
