b064d8a912f2270d375be7309cc04db4c215132f
ChristopherAGT <christopherchivalan01@gmail.com>
2025-06-06 16:08:36 -0600

    Update v2ray-manager.sh

--- v2ray-manager.sh
+++ v2ray-manager.sh
@@ -50,7 +50,7 @@ function mostrar_menu() {
             2) remover_servicio ;;
             3)
                 echo -e "${green}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${nc}"
-                echo -e "${yellow}Saliendo... xd${nc}"
+                echo -e "${yellow}Saliendo... 👾 Creditos a Leo Duarte${nc}"
                 exit 0
                 ;;
             *) echo -e "${red}Opción inválida. Inténtalo de nuevo.${nc}"; sleep 2 ;;
